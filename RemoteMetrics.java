df7df6f075a3abb1941bf40583ce0c50259be6c7
Yushi Meng <yushim@uber.com>
2020-07-16 11:35:02 -0700

    add RemoteMetrics.java

---
+++ RemoteMetrics.java
@@ -0,0 +1,20 @@
+import java.time.Duration;
+
+import java.time.Duration;
+
+public class RemoteMetrics {
+    private Duration total_time;
+    
+    public RemoteMetrics(Duration total_t, String remote_metrics){
+        total_time = total_t;
+        
+    }
+
+    public static void main(String[] args) throws Exception  
+    {
+        String str = "parse: 0.06%, queue: 10.20%, network: 6.54%, upload: 0.00%, setup: 8.73%, process: 73.72%, fetch: 0.00%, retry: 0.00%, processOutputs: 0.61%, other: 0.04%, input files: 23987, input bytes: 768787483, memory bytes: 0";
+
+
+
+    }    
+}
