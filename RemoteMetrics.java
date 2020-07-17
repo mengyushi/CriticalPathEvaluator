03720dfe8283110863a0ed0dfa7518978d85c420
Yushi Meng <yushim@uber.com>
2020-07-17 12:22:03 -0700

    delete test main

--- RemoteMetrics.java
+++ RemoteMetrics.java
@@ -61,15 +61,4 @@ public class RemoteMetrics {
     public void printSummary() {
         System.out.println(this.summary);
     }
-
-    public static void main(String[] args) throws Exception  
-    {
-        Float t = 10000.0f;
-        String str = "parse: 0.06%, queue: 10.20%, network: 6.54%, upload: 0.00%, setup: 8.73%, process: 73.72%, fetch: 0.00%, retry: 0.00%, processOutputs: 0.61%, other: 0.04%, input files: 23987, input bytes: 768787483, memory bytes: 0";
-        
-        RemoteMetrics rMetrics = createFromString(t, str);
-        System.out.println(rMetrics.getTime("parse"));
-        System.out.println(rMetrics.getTime("other"));
-
-    }    
 }
